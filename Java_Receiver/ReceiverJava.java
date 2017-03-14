4481838ee53b9530cf9b326e4ae87f5a26dc4c31
Philipp Mayer <mayerph@stud.sbg.ac.at>
2017-03-14 11:38:12 +0100

    java receiver mit autoterminierung

--- Java_Receiver/ReceiverJava.java
+++ Java_Receiver/ReceiverJava.java
@@ -3,20 +3,19 @@ import java.net.*;
 import java.util.*;
 
 public class ReceiverJava {
-	final private static int PORT = 1234; // arbitrarily assigned port to use
-
 	public static void main(String args[]) throws IOException {
-		DatagramSocket socket = new DatagramSocket(PORT);
-		for (int x = 0; x < 100; x++) {
-			byte buffer[] = new byte[256];
+		int port=Integer.parseInt(args[0]);
+		byte[] buffer;
+		DatagramSocket socket = new DatagramSocket(port);
+		while (true) {
+			buffer = new byte[256];
 			DatagramPacket packet = new DatagramPacket(buffer, buffer.length); 
 			socket.receive(packet);
 			String clientBMsg = new String(packet.getData());
+			clientBMsg=clientBMsg.trim();
+			if(clientBMsg.equals("EndOfTransmission")) break;
 			System.out.println(clientBMsg);
 			InetAddress address = packet.getAddress();
-			buffer = null;
-			String msgString = "Clinet A";
-			buffer = msgString.getBytes();
 		}
 		socket.close();
 	}
