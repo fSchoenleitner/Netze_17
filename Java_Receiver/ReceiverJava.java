22747cbd651f92788d03f0f09455f62c878bec45
Philipp Mayer <mayerph@stud.sbg.ac.at>
2017-03-26 16:57:34 +0200

    receiver ohne zeitmessung

--- Java_Receiver/ReceiverJava.java
+++ Java_Receiver/ReceiverJava.java
@@ -6,41 +6,25 @@ public class ReceiverJava {
 	public static void main(String args[]) throws IOException {
 		int port=Integer.parseInt(args[0]);
 		int counter=0;
+		byte[] buffer;
+		String adrStr;
 		DatagramSocket socket = new DatagramSocket(port);
-		BufferedWriter bf=initWriter(args[1]);
 		while (true) {
-			DatagramPacket packet=receive(socket, bf);
+			buffer = new byte[256];
+			DatagramPacket packet = new DatagramPacket(buffer, 256); 
+			socket.receive(packet);
 			String clientBMsg = new String(packet.getData());
 			clientBMsg=clientBMsg.trim();
 			if(clientBMsg.equals("EndOfTransmission")) break;
 			counter++;
-			System.out.println(clientBMsg);
+			
 			InetAddress address = packet.getAddress();
+			adrStr=address.getHostAddress();
+			System.out.println("Received Message: \""+clientBMsg+"\" from: "+adrStr+":"+Integer.toString(port));
+			System.out.println();
 		}
 		socket.close();
-		bf.close();
 		System.out.println(counter+" packets received");
 	}
 	
-	private static DatagramPacket receive(DatagramSocket socket, BufferedWriter bf) throws IOException{
-		byte[] buffer = new byte[256];
-		DatagramPacket packet = new DatagramPacket(buffer, buffer.length); 
-		long startTime = System.nanoTime();
-		socket.receive(packet);
-		long endTime = System.nanoTime();
-		long duration=endTime-startTime;
-		String durStr=Long.toString(duration);
-		bf.write(durStr, 0, durStr.length());
-		bf.newLine();
-		return packet;
-	}
-	
-	private static BufferedWriter initWriter(String filename) throws IOException{
-		File f=new File(filename);
-		FileWriter osw=new FileWriter(f);
-		BufferedWriter bf=new BufferedWriter(osw);
-		bf.write("Receiving duration [ns]", 0, 23);
-		bf.newLine();
-		return bf;
-	}
 }
