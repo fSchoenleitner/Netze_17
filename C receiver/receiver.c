73a56246807aa3ce824c0d98a071f7f4aba84284
Philipp Mayer <mayerph@stud.sbg.ac.at>
2017-03-27 19:18:04 +0200

    receiver ohne zeitmessung

--- C receiver/receiver.c
+++ C receiver/receiver.c
@@ -15,9 +15,6 @@ int main(int argc, char *argv[]){
     int slen , recv_len, buflen=atoi(argv[2]), port=atoi(argv[1]);
     char buf[buflen];
     WSADATA wsa;
-	SYSTEMTIME end_time, start_time;
-	long end_millis, start_millis, rec_millis, rec_duration, transm_duration;
-	char *millis;
 	printf("port: %d  buflen: %d\n", port, buflen);
     slen = sizeof(addr) ;
      
@@ -47,6 +44,7 @@ int main(int argc, char *argv[]){
     printf("Bind done\n");
 
 	int i=1;
+	int counter=0;
     while(i){
         printf("Waiting...");
         fflush(stdout);
@@ -55,26 +53,22 @@ int main(int argc, char *argv[]){
         memset(buf,'\0', buflen);
          
         //try to receive some data, this is a blocking call
-        GetSystemTime(&start_time);
 		if ((recv_len = recv(s, buf, buflen, 0)) == SOCKET_ERROR){
             printf("recvfrom() failed with error code : %d" , WSAGetLastError());
             exit(EXIT_FAILURE);
         }
-		GetSystemTime(&end_time);
-		start_millis=(start_time.wSecond * 1000) + start_time.wMilliseconds;
-		end_millis=(end_time.wSecond * 1000) + end_time.wMilliseconds;
-		rec_duration=end_millis-start_millis;
          
         //print details of the client/peer and the data received
 		i=strcmp(buf, "EndOfTransmission");
 		if(i){
-			millis=strstr(buf, "at ");
-			*millis=*(millis+4);
-			transm_duration=atoi(millis);
-			printf("Received packet from %s:%d in time %d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), transm_duration);
-			printf("Packet: %s\n" , buf);
+			counter++;
+			printf("received message:\n");
+			printf(buf);
+			printf("\n");
+			printf("\n");
 		}else{
-			printf("Transmission terminated!\n");
+			printf("Transmission terminated\n");
+			printf("%d packages received...", counter);
 		}
 		
     }
