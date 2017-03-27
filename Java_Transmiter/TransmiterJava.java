12b31f2249e9c1120393a77019f37b0076ded2cc
Fabian Schoenleitner <s1020682@stud.sbg.ac.at>
2017-03-27 19:06:08 +0200

    update

--- Java_Transmiter/TransmiterJava.java
+++ Java_Transmiter/TransmiterJava.java
@@ -3,7 +3,7 @@ import java.net.*;
 
 public class TransmiterJava {
 	public static void main(String args[]) throws IOException {
-		System.out.println("Eingabe ist: PORT | ANZAHL DER PAKETE |  Host = localhost");
+		//Eingabe ist: PORT | ANZAHL DER PAKETE |  Host = localhost
 		final int PORT = Integer.parseInt(args[0]); 
 		String host = args[2]; //"localhost"
 		InetAddress address = InetAddress.getByName(host);
@@ -13,11 +13,11 @@ public class TransmiterJava {
 		for (int x = 1; x <= Integer.parseInt(args[1]); x++) {
 			System.out.println();
 			System.out.println("Sending to: " + address+":"+PORT);
-			msgString="Message "+x;			
+			msgString= Integer.toString(x);	
 			message = msgString.getBytes();
 			DatagramPacket packet = new DatagramPacket(message, message.length, address, PORT);
 			socket.send(packet);
-			System.out.println(msgString);
+			System.out.println(msgString + "Message sent");
 		}
 		sendTerminationString(address, PORT, socket);
 	}
